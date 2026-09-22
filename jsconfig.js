93c662e2604cf6b5ba9a3dc47502edf68f3b897d
emelenaramirez-lgtm <emelenaramirez@gmail.com>
2026-09-22 09:38:35 -0500

    Delete jsconfig.js

--- jsconfig.js
+++
@@ -1,4 +0,0 @@
-window.APP_CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxbdztshEhOPbCr9hmKq8J5DqMnZlzdX1Or61Z5A9gFtfYvoKI4cKpeh4mNh40hLKKM/exec',
-  APP_VERSION: '1.0.0'
-};
